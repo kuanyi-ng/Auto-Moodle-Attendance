bf8fdc25941ea7caf2f26b3c9165784a94d463fb
KY-Ng <raionkurane@gmail.com>
2020-02-15 15:31:56 +0900

    Implemented login()

--- helper_functions.py
+++ helper_functions.py
@@ -1,2 +1,12 @@
 #! python 3
 #! utf-8
+
+def login(browser, username, password):
+    # get DOM elements
+    usernameInput = browser.find_element_by_id('username')
+    passwordInput = browser.find_element_by_id('password')
+    loginButton = browser.find_element_by_id('loginbtn')
+    # input
+    usernameInput.send_keys(username)
+    passwordInput.send_keys(password)
+    loginButton.click()
--- take_attendance.py
+++ take_attendance.py
@@ -3,15 +3,16 @@
 
 import bs4
 import datetime
+import getpass
 import re
 import requests
-import selenium
 
+from selenium import webdriver
 from helper_functions import *
 
 # Variables
 moodleURL = "https://moodle.s.kyushu-u.ac.jp/"
-
+loginURL = "https://moodle.s.kyushu-u.ac.jp/login/index.php"
 """
 ====== Automation of Attendance Taking on Moodle =====
 | Timetable |
@@ -32,8 +33,18 @@ if __name__ == "__main__":
         # Update Timetable
         # Register Course on Moodle
         # if no arguments, take attendance
-    # 1. Access Moodle
-    # 2. Log In
+    # Get username and Password
+    username = input('Username: ')
+    pw = getpass.getpass('Password: ')
+    # Access Moodle Log In Page
+    browser = webdriver.Firefox()
+    browser.get(loginURL)
+    # Log In
+    login(browser, username, pw)
+
     # 3. Access Course Page
     # 4. Access Attentance Page (of the Course)
     # 5. Take Attendance
+
+    # Quit
+    browser.quit()
