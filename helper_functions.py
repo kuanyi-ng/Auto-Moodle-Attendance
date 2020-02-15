9c45fb2fad1ce8303cb4364e368904baeac6f785
KY-Ng <raionkurane@gmail.com>
2020-02-15 19:37:46 +0900

    added attend() and search()

--- helper_functions.py
+++ helper_functions.py
@@ -17,12 +17,8 @@ loginURL = "https://moodle.s.kyushu-u.ac.jp/login/index.php"
 - has same content with Timetable (excel) but also comes with Subject ID
 """
 
-def login(browser, username, password):
-    # get DOM elements
-    usernameInput = browser.find_element_by_id('username')
-    passwordInput = browser.find_element_by_id('password')
-    loginButton = browser.find_element_by_id('loginbtn')
-    # input
-    usernameInput.send_keys(username)
-    passwordInput.send_keys(password)
-    loginButton.click()
+def attend():
+    pass()
+
+def search():
+    pass()
