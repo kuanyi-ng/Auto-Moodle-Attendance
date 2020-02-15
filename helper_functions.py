1cbcbf758a337fc68f9bc990661fd0b18d3a31de
KY-Ng <raionkurane@gmail.com>
2020-02-15 19:32:37 +0900

    move part of the codes from main.py to helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,22 @@
 #! python 3
 #! utf-8
 
+""" Variables """
+moodleURL = "https://moodle.s.kyushu-u.ac.jp/"
+loginURL = "https://moodle.s.kyushu-u.ac.jp/login/index.php"
+
+"""
+====== Automation of Attendance Taking on Moodle =====
+| Timetable |
+- excel file
+- editable by user
+
+| Timetable (csv) |
+- read-only (user)
+- generated / updated with Python based on Timetable
+- has same content with Timetable (excel) but also comes with Subject ID
+"""
+
 def login(browser, username, password):
     # get DOM elements
     usernameInput = browser.find_element_by_id('username')
--- main.py
+++ main.py
@@ -2,30 +2,12 @@
 #! utf-8
 
 import bs4
-import datetime
 import getpass
 import re
 import requests
 import sys
 
-from selenium import webdriver
 from helper_functions import *
-
-# Variables
-moodleURL = "https://moodle.s.kyushu-u.ac.jp/"
-loginURL = "https://moodle.s.kyushu-u.ac.jp/login/index.php"
-"""
-====== Automation of Attendance Taking on Moodle =====
-| Timetable |
-- excel file
-- editable by user
-
-| Timetable (csv) |
-- read-only (user)
-- generated / updated with Python based on Timetable
-- has same content with Timetable (excel) but also comes with Subject ID
-"""
-
 
 if __name__ == "__main__":
     # get argument from terminal
